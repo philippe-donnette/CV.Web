25d35d0aaea79d8f949045040c2a13d6f1a09762
Philippe Donnette <philippe.donnette@gmail.com>
2016-03-09 05:17:58 +0000

    Added directive to list versions for skills

--- src/app/controllers/skillsController.js
+++ src/app/controllers/skillsController.js
@@ -1,11 +1,9 @@
 app.controller('skillsController', ['$scope', 'skillsService', function ($scope, skillsService) {
     
-    
     skillsService.getSkills().then(
         function (response) {
             if(response.status == 200) {
-                $scope.tags = response.data;
-                console.log(JSON.stringify(response));    
+                $scope.tags = response.data;    
             }
             else {
                 $scope.tags = [];
@@ -15,24 +13,5 @@ app.controller('skillsController', ['$scope', 'skillsService', function ($scope,
             $scope.tags = [];
         }
     );
-    
-    
-    /*
-    $scope.tags = [
-        { label: "AngularJS", weight: "10" },
-        { label: "HTML", weight: "5" },
-        { label: "Bootstrap", weight: "8" },
-        { label: "LESS", weight: "8" },
-        { label: "CSS", weight: "5" },
-        { label: "Jenkins", weight: "3" },
-        { label: "Gulp", weight: "5" },
-        { label: "Bower", weight: "3" },
-        { label: "NPM", weight: "3" },
-        { label: "Visual Studio Code", weight: "10" },
-        { label: "Font Awesome", weight: "4" },
-        { label: "Karma", weight: "6" },
-        { label: "Jasmine", weight: "6" },
-        { label: "TDD", weight: "5" }
-    ];*/
     
 }]);
