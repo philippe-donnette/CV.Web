b49f641d4b66a9283476b22472720221c2577414
Philippe Donnette <philippe.donnette@gmail.com>
2016-03-20 12:04:56 +0000

    Hide photo for small screens

--- src/app/controllers/headerController.js
+++ src/app/controllers/headerController.js
@@ -1,12 +1,5 @@
 app.controller('headerController', ['$scope', 'projectService', function ($scope, projectService) {
       
-    /*    
-    $scope.projects = [
-        { href: "root.project({ id: 10, title: 'cv' })", title: "cv", id: 1 },
-        { href: "root.project({ id: 8, title: 'deptford-park-football' })", title: "Deptford Park Football", id: 2 }
-    ];
-    */
-    
     $scope.init = function () {
         projectService.getProjects().then(
             function (response) {
