9406eeabbf8228fbd63b396a0ec27e4f7fd6b771
Philippe Donnette <philippe.donnette@gmail.com>
2016-03-24 15:49:10 +0000

    Refactoring UI

--- src/app/controllers/qualificationsController.js
+++ src/app/controllers/qualificationsController.js
@@ -10,8 +10,32 @@ app.controller('qualificationsController', ['$scope', 'qualificationService', fu
                     $scope.studies = null;
                     
                     $scope.studies = [
-                        { id: 1, schoolName: "H.E.I.G" },
-                        { id: 2, schoolName: "Martin Luther King" } 
+                        { 
+                            id: 1, 
+                            schoolName: "H.E.I.G", 
+                            startDate: new Date(2003, 9, 1), 
+                            endDate: new Date(2005, 7, 31),
+                            imageUrl: 'heig.png',
+                            city: 'Paris',
+                            country: 'France',
+                            subject: 'BTS informatique de gestion',
+                            description: '<p>some blah blah, some blah blah, some blah blah, some blah blah</p>',
+                            websiteUrl: null,
+                            degreeFile: 'heig.pdf'
+                        },
+                        { 
+                            id: 2, 
+                            schoolName: "Martin Luther King",
+                            startDate: new Date(1998, 9, 1), 
+                            endDate: new Date(2000, 7, 31),
+                            imageUrl: null,
+                            city: 'Paris',
+                            country: 'France',
+                            subject: 'Terminal S technology',
+                            description: '<p>some blah blah, some blah blah, some blah blah, some blah blah</p>',
+                            websiteUrl: 'http://www.google.co.uk',
+                            degreeFile: null
+                        } 
                     ];
                 }
             );
